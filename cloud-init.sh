30d31e27c169d18a6e394d476b9d9d72c852eb5b
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-12-09 13:53:58 +0000

    cloud-init: remove custom docker

--- cloud-init.sh
+++ cloud-init.sh
@@ -17,15 +17,6 @@ EOF
 apt-get update
 apt-get install -y curl jq git ca-certificates gnupg lsb-release
 
-# docker
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-chmod a+r /etc/apt/keyrings/docker.gpg
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-usermod -a -G docker ubuntu
-
 # github runner
 cd /
 rm -rf github
