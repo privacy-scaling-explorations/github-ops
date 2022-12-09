f253c8b4c65620c27e83be1fbeac0e7b65bb0e9f
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-12-09 13:26:36 +0000

    support both spot & on demand

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ for WORKFLOW_ID in $QUEUED; do
     EXIT_CODE=1
     continue
   fi
+  echo 'deploying'
 
   JOB_LABELS=$(echo "${JOB_DATA}" | jq -cr '.jobs[].labels')
   # skip if not self hosted
@@ -20,17 +21,19 @@ for WORKFLOW_ID in $QUEUED; do
   JOB_ATTEMPTS=$(echo "${JOB_DATA}" | jq -cr '.jobs[].run_attempt')
   INSTANCE_TYPE=$(echo "${JOB_LABELS}" | jq -cr '.[2]')
   TAG="${REPO}-${WORKFLOW_ID}"
-  INSTANCES_STATUS=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${TAG}" | jq -cr '.SpotInstanceRequests[].State')
-  # just in case we somehow ended up with multiple machines with the same id
-  if [ "${INSTANCES_STATUS}" != "" ] && [ "$(echo "${INSTANCES_STATUS}" | grep -q -E '(open|active)'; echo "$?")" = "0" ]; then
-    echo 'already deployed'
-    continue
-  fi
-
-  echo 'deploying'
   RUNNER_LABELS=$(echo "${JOB_LABELS}" | jq -cr 'join(",")')
   USER_DATA=$(cat cloud-init.sh | sed -e "s#__REPO__#${REPO}#" -e "s/__RUNNER_LABELS__/${RUNNER_LABELS}/" -e "s/__GITHUB_TOKEN__/${GH_PAT}/" | base64 -w 0)
-  JSON=$(
+
+  IS_SPOT=$(echo "${JOB_LABELS}" | grep -qv "spot"; echo "$?")
+  if [ "${IS_SPOT}" = "1" ]; then
+    INSTANCES_STATUS=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${TAG}" | jq -cr '.SpotInstanceRequests[].State')
+    # just in case we somehow ended up with multiple machines with the same id
+    if [ "${INSTANCES_STATUS}" != "" ] && [ "$(echo "${INSTANCES_STATUS}" | grep -q -E '(open|active)'; echo "$?")" = "0" ]; then
+      echo 'already deployed'
+      continue
+    fi
+
+    JSON=$(
 cat | jq -cr '.' << EOF
 {
   "UserData": "${USER_DATA}",
@@ -47,14 +50,36 @@ cat | jq -cr '.' << EOF
 EOF
 )
 
-  # continue on error
-  aws ec2 request-spot-instances \
-    --type one-time \
-    --instance-interruption-behavior terminate \
-    --instance-count 1 \
-    --tag-specification "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${TAG}}]" \
-    --client-token "${TAG}-${JOB_ATTEMPTS}" \
-    --launch-specification "${JSON}" || EXIT_CODE=1
+    # continue on error
+    aws ec2 request-spot-instances \
+      --type one-time \
+      --instance-interruption-behavior terminate \
+      --instance-count 1 \
+      --tag-specification "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${TAG}}]" \
+      --client-token "${TAG}-${JOB_ATTEMPTS}" \
+      --launch-specification "${JSON}" || EXIT_CODE=1
+
+  else
+    INSTANCES_STATUS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${TAG}" | jq -cr '.Reservations[].Instances[].State.Name')
+    # just in case we somehow ended up with multiple machines with the same id
+    if [ "${INSTANCES_STATUS}" != "" ] && [ $(echo "${INSTANCES_STATUS}" | grep -v terminated) ]; then
+      echo 'already deployed'
+      continue
+    fi
+
+    # continue on error
+    aws ec2 run-instances \
+      --user-data "${USER_DATA}" \
+      --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 64, \"DeleteOnTermination\": true } } ]" \
+      --ebs-optimized \
+      --instance-initiated-shutdown-behavior terminate \
+      --instance-type "${INSTANCE_TYPE}" \
+      --image-id "${IMAGE_ID}" \
+      --key-name "${KEY_NAME}" \
+      --subnet-id "${SUBNET_ID}" \
+      --security-group-id "${SECURITY_GROUP_ID}" \
+      --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${TAG}}]" || EXIT_CODE=1
+  fi
 done
 
 # cleanup if no jobs are in progress nor queued
@@ -73,6 +98,18 @@ for VAL in $RES; do
     if [ "${INSTANCE_ID}" != "null" ]; then
       aws ec2 terminate-instances --instance-ids "${INSTANCE_ID}" || EXIT_CODE=1
     fi
+  fi
+done
+
+# cleanup for non-spot instances
+RES=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${REPO}-*" "Name=instance-state-name,Values=running" | jq -cr '.Reservations[].Instances[] | [.InstanceId, .Tags[0].Value]')
+for VAL in $RES; do
+  ID=$(echo "${VAL}" | jq -cr '.[0]')
+  TAG=$(echo "${VAL}" | jq -cr '.[1]')
+  WORKFLOW_ID=$(echo "${TAG}" | awk -F '-' '{ print $NF }')
+  JOB_STATUS=$(curl -H "authorization: token ${GH_PAT}" "https://api.github.com/repos/${REPO}/actions/runs/${WORKFLOW_ID}" | jq -cr '.status')
+  if [ "${JOB_STATUS}" != "queued" ] && [ "${JOB_STATUS}" != "in_progress" ]; then
+    aws ec2 terminate-instances --instance-ids "${ID}"
   fi
 done
 
