15efda5c709ceca4a185f731159485c6b9df3841
Nick <nick@gmx.co.uk>
2023-09-28 13:42:00 +0300

    Fix typo

--- run.sh
+++ run.sh
@@ -119,7 +119,7 @@ for VAL in $RES; do
   JOB_STATUS=$(curl -H "authorization: token ${GH_PAT}" "https://api.github.com/repos/${REPO}/actions/jobs/${JOB_ID}" | jq -cr '.status')
   if [ "${JOB_STATUS}" != "queued" ] && [ "${JOB_STATUS}" != "in_progress" ]; then
     EC2_LAUNCH_TIME=$(aws ec2 describe-instances --instance-ids "${ID}" --query 'Reservations[*].Instances[*].LaunchTime' --output text)
-    LAUNCH_TIME_EPOCH=$(date -d $EC2_LAUNCH_TIME +"%s"
+    LAUNCH_TIME_EPOCH=$(date -d $EC2_LAUNCH_TIME +"%s")
     diff=$(expr $CURRENT_TIME_EPOCH - $LAUNCH_TIME_EPOCH)
     if [ $diff -gt 5400 ]; then
       aws ec2 terminate-instances --instance-ids "${ID}"
